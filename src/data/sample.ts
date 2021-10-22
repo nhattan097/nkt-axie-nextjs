493085ab2aba378c07562931472777b738f635e8
Nhật Tân <tan.vn@appfast.io>
2021-10-22 17:23:21 +0700

    data sample

---
+++ src/data/sample.ts
@@ -0,0 +1,99 @@
+export const best_players = {
+  players: [
+    {
+      id: 1,
+      name: 'Player Name',
+      no: 'No.2019',
+      server_name: 'Adrian (G01)',
+      pets: [
+        {
+          id: '#672920',
+          color_code: '#FFAF13',
+          type: 'land',
+        },
+        {
+          id: '#672921',
+          color_code: '#00AFC7',
+          type: 'water',
+        },
+        {
+          id: '#672922',
+          color_code: '#80BD39',
+          type: 'fly',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Player Name',
+      no: 'No.2019',
+      server_name: 'Adrian (G01)',
+      pets: [
+        {
+          id: '#672920',
+          color_code: '#FFAF13',
+          type: 'land',
+        },
+        {
+          id: '#672921',
+          color_code: '#00AFC7',
+          type: 'water',
+        },
+        {
+          id: '#672922',
+          color_code: '#80BD39',
+          type: 'fly',
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Player Name',
+      no: 'No.2019',
+      server_name: 'Adrian (G01)',
+      pets: [
+        {
+          id: '#672920',
+          color_code: '#FFAF13',
+          type: 'land',
+        },
+        {
+          id: '#672921',
+          color_code: '#00AFC7',
+          type: 'water',
+        },
+        {
+          id: '#672922',
+          color_code: '#80BD39',
+          type: 'fly',
+        },
+      ],
+    },
+  ],
+};
+
+export const best_team = {
+  player: {
+    name: 'Jessie (G11)',
+  },
+  pets: [
+    {
+      id: '#672920',
+      name: 'NKT-G',
+      color: 'yellow',
+      color_code: '#FFAF13',
+    },
+    {
+      id: '#672921',
+      name: 'NKT-G',
+      color: 'blue',
+      color_code: '#00AFC7',
+    },
+    {
+      id: '#672922',
+      name: 'NKT-G',
+      color: 'green',
+      color_code: '#80BD39',
+    },
+  ],
+};

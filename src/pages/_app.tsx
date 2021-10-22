04a59145a811b925d51babf91bfb55b61eed17c3
Nhật Tân <tan.vn@appfast.io>
2021-10-22 17:23:55 +0700

    import swipe css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '~/theme';
 
+// styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '~/styles/swipe.css';
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>

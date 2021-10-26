57cd33fbd814455bc2121fb8d6f279b677ecd3dc
Nhật Tân <tan.vn@appfast.io>
2021-10-26 16:44:32 +0700

    add svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,17 @@
+import AccountCircle from './account-circle.svg';
+import Dashboard from './routes/dashboard.svg';
+import Setting from './routes/setting.svg';
+import Axie from './routes/axie.svg';
+import Scholar from './routes/scholar.svg';
+import Axs from './coin/axs.svg';
+import Slp from './coin/slp.svg';
+import Eth from './coin/eth.svg';
+
+export const AccountCircleSVG = AccountCircle;
+export const DashboardSVG = Dashboard;
+export const SettingSVG = Setting;
+export const AxieSVG = Axie;
+export const ScholarSVG = Scholar;
+export const AxsSVG = Axs;
+export const SlpSVG = Slp;
+export const EthSVG = Eth;
